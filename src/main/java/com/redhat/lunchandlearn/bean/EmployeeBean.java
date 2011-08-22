21c4a301d7752d60c8e15c08314332e24a22a3b6
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2011-08-21 21:26:00 -0300

    Finishing Richfaces CRUD example

--- src/main/java/com/redhat/lunchandlearn/bean/EmployeeBean.java
+++ src/main/java/com/redhat/lunchandlearn/bean/EmployeeBean.java
@@ -2,7 +2,8 @@ package com.redhat.lunchandlearn.bean;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 import org.jboss.forge.persistence.PersistenceUtil;
@@ -10,7 +11,8 @@ import org.jboss.forge.persistence.PersistenceUtil;
 import com.redhat.lunchandlearn.entity.Employee;
 
 @Named("employeeBean")
-@Stateless
+@Stateful
+@RequestScoped
 public class EmployeeBean extends PersistenceUtil {
 	
 	private List<Employee> list = null;
@@ -18,7 +20,7 @@ public class EmployeeBean extends PersistenceUtil {
 	private Employee employee = new Employee();
 	
 	private long id = 0;
-
+	
 	public List<Employee> getList() {
 		list = findAll(Employee.class);
 		return list;
@@ -28,15 +30,36 @@ public class EmployeeBean extends PersistenceUtil {
 		this.list = list;
 	}
 	
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public String load(Integer id) {
+		Long longId = new Long(id + "");
+		employee = findById(Employee.class, longId);
+		return "load";
+	}
+	
 	public String create() {
+		getEntityManager().joinTransaction();
+		getEntityManager().persist(employee);
 		return "create";
 	}
 	
 	public String merge() {
+		getEntityManager().joinTransaction();
+		getEntityManager().merge(employee);
 		return "merge";
 	}
 	
-	public String remove() {
+	public String remove(Integer id) {
+		Long longId = new Long(id + "");
+		getEntityManager().joinTransaction();
+		getEntityManager().remove(findById(Employee.class, longId));
 		return "remove";
 	}
 
--- src/main/java/com/redhat/lunchandlearn/entity/Employee.java
+++ src/main/java/com/redhat/lunchandlearn/entity/Employee.java
@@ -1,5 +1,4 @@
 package com.redhat.lunchandlearn.entity;
-
 import javax.persistence.Entity;
 import java.io.Serializable;
 import javax.persistence.Id;
@@ -8,57 +7,34 @@ import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
-
-@Entity
-public class Employee implements java.io.Serializable {
-	
-	@Id
-	private @GeneratedValue(strategy = GenerationType.AUTO)
-	
-	@Column(name = "id", updatable = false, nullable = false)
-	Long id = null;
-	
-	@Version
-	private @Column(name = "version")
-	int version = 0;
-
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(final Long id) {
-		this.id = id;
-	}
-
-	public int getVersion() {
-		return this.version;
-	}
-
-	public void setVersion(final int version) {
-		this.version = version;
-	}
-
-	@NotNull
-	@Column
-	private Integer number;
-
-	public Integer getNumber() {
-		return this.number;
-	}
-
-	public void setNumber(final Integer number) {
-		this.number = number;
-	}
-
-	@NotNull
-	@Column
-	private String name;
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(final String name) {
-		this.name = name;
-	}
+import javax.validation.constraints.Max;
+@Entity public class Employee implements java.io.Serializable {
+  @Id private @GeneratedValue(strategy=GenerationType.AUTO) @Column(name="id",updatable=false,nullable=false) Long id=null;
+  @Version private @Column(name="version") int version=0;
+  public Long getId(){
+    return this.id;
+  }
+  public void setId(  final Long id){
+    this.id=id;
+  }
+  public int getVersion(){
+    return this.version;
+  }
+  public void setVersion(  final int version){
+    this.version=version;
+  }
+  @NotNull @Column private Integer number;
+  public Integer getNumber(){
+    return this.number;
+  }
+  public void setNumber(  final Integer number){
+    this.number=number;
+  }
+  @NotNull @Column private String name;
+  public String getName(){
+    return this.name;
+  }
+  public void setName(  final String name){
+    this.name=name;
+  }
 }
